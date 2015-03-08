83d294ece3683895a25c5f2be2c7b5850369f936
Brecht De Meulenaere <brecht_de_meulenaere@outlook.com>
2015-03-08 13:04:07 +0100

    PCMD command: flag must be int

--- app/drones/models/ATCommand/ATCommandPCMD.java
+++ app/drones/models/ATCommand/ATCommandPCMD.java
@@ -7,13 +7,13 @@ public class ATCommandPCMD extends ATCommand {
     private static final String TYPE = "AT*PCMD";
 
     // Params command REF
-    private float flag;
+    private int flag;
     private float roll;
     private float pitch;
     private float gaz;
     private float yaw;
 
-    public ATCommandPCMD(float flag, float roll, float pitch, float gaz, float yaw) {
+    public ATCommandPCMD(int flag, float roll, float pitch, float gaz, float yaw) {
         this.flag = flag;
         this.roll = roll;
         this.pitch = pitch;
@@ -23,7 +23,7 @@ public class ATCommandPCMD extends ATCommand {
 
     @Override
     public String toString() {
-        String params = intOfFloat(flag) + "," + intOfFloat(roll) + "," + intOfFloat(pitch) + "," + intOfFloat(gaz) + "," + intOfFloat(yaw);
+        String params = flag + "," + intOfFloat(roll) + "," + intOfFloat(pitch) + "," + intOfFloat(gaz) + "," + intOfFloat(yaw);
         return (TYPE + "=" + seq + "," + params + "\r");
     }
 }
