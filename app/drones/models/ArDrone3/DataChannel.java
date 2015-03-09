e560836786b9abc43cd6f579028528e0ed3a14c9
Cedric Van Goethem <vangoethem.c@gmail.com>
2015-03-09 12:28:51 +0100

    Added ack channel support

--- app/drones/models/ArDrone3/DataChannel.java
+++ app/drones/models/ArDrone3/DataChannel.java
@@ -2,6 +2,7 @@ package drones.models.ardrone3;
 
 import akka.util.ByteString;
 
+import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
@@ -23,7 +24,12 @@ public class DataChannel {
     private int numRetry;
 
     //Concurrency primitives
-  //  private ConcurrentLinkedQueue<AckedFrame> frameQueue;
+    private LinkedList<Frame> frameQueue; //TODO: use locked queue, no concurrent
+    private long lastSend = 0;
+    private int retried = 0;
+
+    private int missed = 0; //TODO: statistics
+    private int sent = 0;
 
     public DataChannel(byte id, FrameType type) {
         this(id, type, 20, -1, INFINITE_RETRY);
@@ -36,13 +42,43 @@ public class DataChannel {
 
         this.sendDelay = sendDelay;
         this.ackTimeout = ackTimeout;
-        this.numRetry = numRetry;
+        this.numRetry = numRetry + 1;
 
-       /* if (type == FrameType.DATA_WITH_ACK) { //Create a sender buffer if necesary
-            this.frameQueue = new ConcurrentLinkedQueue<>();
+        if (type == FrameType.DATA_WITH_ACK) { //Create a sender buffer if necesary
             if (numRetry == INFINITE_RETRY)
                 throw new IllegalArgumentException("numRetry cannot be infinite with ACK channel");
-        }*/
+            this.frameQueue = new LinkedList<>();
+        }
+    }
+
+    public Frame sendFrame(Frame f, long time){
+        synchronized (lock){
+            frameQueue.offer(f);
+        }
+        return tick(time);
+    }
+
+    public Frame tick(long time){
+        synchronized (lock){
+            if(!frameQueue.isEmpty()){
+                long diff = time - lastSend;
+                if(diff > ackTimeout){
+                    retried++;
+                    if(retried > numRetry){
+                        frameQueue.poll(); // pop frame
+                        retried = 0;
+                    }
+                    lastSend = time;
+                    if(frameQueue.isEmpty()) {
+                        lastSend = 0; //enforce send next time
+                        return null;
+                    } else {
+                        return frameQueue.peek();
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     public boolean shouldAllowFrame(Frame f){
@@ -66,7 +102,16 @@ public class DataChannel {
     }
 
     public void receivedAck(byte seq){
-
+        synchronized (lock){
+            Frame f = frameQueue.peek();
+            if(f.getSeq() == seq){
+                lastSend = 0;
+                retried = 0;
+                frameQueue.poll();
+            } else {
+//TODO: log!
+            }
+        }
     }
 
     public Frame createFrame(ByteString data){
