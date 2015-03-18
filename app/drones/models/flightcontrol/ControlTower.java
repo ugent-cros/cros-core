9fbf6ad659a21ce533a982e47d5bc43e594eca56
Sander Ballieu <sander.ballieu@ugent.be>
2015-03-18 17:02:14 +0100

    FlightControl classes

---
+++ app/drones/models/flightcontrol/ControlTower.java
@@ -0,0 +1,9 @@
+package drones.models.flightcontrol;
+
+/**
+ * Created by Sander on 18/03/2015.
+ *
+ * Flight control to control multiple drones
+ */
+public abstract class ControlTower extends FlightControl{
+}
---
+++ app/drones/models/flightcontrol/Pilot.java
@@ -0,0 +1,18 @@
+package drones.models.flightcontrol;
+
+import drones.models.DroneCommander;
+
+/**
+ * Created by Sander on 18/03/2015.
+ *
+ * Flight control for one single drone = pilot of the drone.
+ */
+public abstract class Pilot extends FlightControl{
+
+    private DroneCommander dc;
+
+    public Pilot(DroneCommander dc) {
+        super();
+        this.dc = dc;
+    }
+}
