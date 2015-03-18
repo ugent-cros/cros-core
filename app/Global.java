19da44d2015faadd4bcb3c41ed55639e8b2d7cc4
Ronald Gerard <Ronald.Gerard@UGent.be>
2015-03-18 23:26:43 +0100

    -Global reference to scheduler with onStart/onStop.
    -Changed FlightControls to be the children of scheduler.

--- app/Global.java
+++ app/Global.java
@@ -1,4 +1,9 @@
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import drones.models.scheduler.SimpleScheduler;
+import play.Application;
 import play.GlobalSettings;
+import play.libs.Akka;
 
 /**
  * Created by matthias on 21/02/2015.
@@ -6,8 +11,35 @@ import play.GlobalSettings;
 public class Global extends GlobalSettings {
 
     @Override
-    public void onStart(play.Application application) {
+    public void onStart(Application application) {
         super.onStart(application);
+        startDroneScheduler();
+    }
+
+    @Override
+    public void onStop(Application application) {
+        super.onStop(application);
+        stopDroneScheduler();
+    }
+
+
+    private static ActorRef scheduler;
+
+    public static void startDroneScheduler(){
+        if(scheduler != null) return;
+        scheduler = Akka.system().actorOf(Props.create(SimpleScheduler.class),"Scheduler");
+    }
+
+    public static void stopDroneScheduler(){
+        Akka.system().stop(scheduler);
+    }
+
+    /**
+     * Get an actor reference to the drone scheduler
+     * @return
+     */
+    public static ActorRef getDroneScheduler(){
+        return scheduler;
     }
 
 }
