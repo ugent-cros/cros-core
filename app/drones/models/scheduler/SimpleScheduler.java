fd21d8aef6f0dc447afeabec29c2db67be0e204e
Sander Ballieu <sander.ballieu@ugent.be>
2015-03-19 19:55:00 +0100

    Scheduler fix

--- app/drones/models/scheduler/SimpleScheduler.java
+++ app/drones/models/scheduler/SimpleScheduler.java
@@ -59,7 +59,7 @@ public class SimpleScheduler extends Scheduler{
         // Create SimplePilot
         ActorRef pilot = getContext().actorOf(
                 Props.create(SimplePilot.class,
-                        () -> new SimplePilot(commander)));
+                        () -> new SimplePilot(drone,null)));
         // Tell the pilot to start the flight
         pilot.tell(new StartFlightControlMessage(),self());
     }
