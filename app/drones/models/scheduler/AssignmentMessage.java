954e284339de94ff9b1444cb21d36904fcf80800
Ronald Gerard <Ronald.Gerard@UGent.be>
2015-03-24 15:02:04 +0100

    Small update SimpleScheduler:
    -Init and batteryPercentage checked

--- app/drones/models/scheduler/AssignmentMessage.java
+++ app/drones/models/scheduler/AssignmentMessage.java
@@ -2,10 +2,12 @@ package drones.models.scheduler;
 
 import models.Assignment;
 
+import java.io.Serializable;
+
 /**
  * Created by Ronald on 18/03/2015.
  */
-public class AssignmentMessage {
+public class AssignmentMessage implements Serializable{
 
     private Assignment assignment;
 
