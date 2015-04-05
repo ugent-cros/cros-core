207ddd23f008280ab36abc873d6107af5f89abc6
Sander Ballieu <sander.ballieu@ugent.be>
2015-04-05 11:31:20 +0200

    Start test

--- test/SimplePilotTest.java
+++ test/SimplePilotTest.java
@@ -1,5 +1,23 @@
+import akka.actor.ActorSystem;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+//import akka.testkit.JavaTestKit;
+
 /**
  * Created by Sander on 5/04/2015.
  */
 public class SimplePilotTest {
+
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        //JavaTestKit.shutdownActorSystem(system);
+        system = null;
+    }
 }
