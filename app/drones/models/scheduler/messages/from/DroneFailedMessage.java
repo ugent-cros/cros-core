52df3acd589b8f04793c8ee2710cd76844c44009
Ronald Gerard <Ronald.Gerard@UGent.be>
2015-04-21 11:44:22 +0200

    More tests

---
+++ app/drones/models/scheduler/messages/from/DroneFailedMessage.java
@@ -0,0 +1,7 @@
+package drones.models.scheduler.messages.from;
+
+/**
+ * Created by Ronald on 21/04/2015.
+ */
+public class DroneFailedMessage {
+}
---
+++ app/drones/models/scheduler/messages/to/EmergencyMessage.java
@@ -0,0 +1,7 @@
+package drones.models.scheduler.messages.to;
+
+/**
+ * Created by Ronald on 21/04/2015.
+ */
+public class EmergencyMessage {
+}
