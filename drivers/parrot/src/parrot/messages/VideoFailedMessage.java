9ff6e8adc09a3aef58fc8bdc4cd4e22c497c083f
Brecht De Meulenaere <brecht_de_meulenaere@outlook.com>
2015-05-02 16:09:14 +0200

    Vide failed message

--- drivers/parrot/src/parrot/messages/VideoFailedMessage.java
+++ drivers/parrot/src/parrot/messages/VideoFailedMessage.java
@@ -1,7 +1,9 @@
 package parrot.messages;
 
+import java.io.Serializable;
+
 /**
  * Created by brecht on 5/2/15.
  */
-public class VideoFailedMessage {
+public class VideoFailedMessage implements Serializable {
 }
