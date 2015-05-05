7bc07afd0b19ab2fc952a3711fe74b4754f7498c
Sander Ballieu <sander.ballieu@ugent.be>
2015-05-05 23:13:28 +0200

    SimpleControlTower extra test

--- test/ControlTowerTest.java
+++ test/ControlTowerTest.java
@@ -1,3 +1,4 @@
+import CollisionDetector.*;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
@@ -11,6 +12,7 @@ import drones.scheduler.messages.to.FlightCompletedMessage;
 import models.Checkpoint;
 import models.Drone;
 import models.DroneType;
+import models.Location;
 import org.junit.*;
 import scala.concurrent.Await;
 import scala.concurrent.duration.Duration;
@@ -26,6 +28,10 @@ import java.util.List;
 public class ControlTowerTest extends TestSuperclass {
 
     private static final models.Location STERRE = new models.Location(51.0226, 3.71, 0);
+    private static final Location NORTH = new Location(0.01,0,0);
+    private static final Location SOUTH = new Location(-0.01,0,0);
+    private static final Location WEST = new Location(0,-0.01,0);
+    private static final Location CENTER = new Location(0,0,0);
 
     public static final FiniteDuration MAX_DURATION_MESSAGE = Duration.create(10, "seconds");
     public static final FiniteDuration MAX_DURATION_FLYING = Duration.create(180, "seconds");
@@ -100,6 +106,63 @@ public class ControlTowerTest extends TestSuperclass {
                 expectMsgClass(MAX_DURATION_FLYING, FlightCompletedMessage.class);
 
                 simpleControlTower.tell(new StopFlightControlMessage(), getRef());
+            }
+        };
+    }
+
+
+    /**
+     * Test with 2 drones
+     */
+    @Test
+    public void flyTwoDrones() throws Exception {
+        ArrayList<Long> drones = new ArrayList<>();
+
+        //Drone that will fly from Nord to South
+        Drone droneNS = addDrone(NORTH);
+        drones.add(droneNS.getId());
+        //Drone that will fly from West to Center
+        Drone droneWC = addDrone(WEST);
+        drones.add(droneWC.getId());
+
+        List<Checkpoint> wayPointsDroneNS = new ArrayList<>();
+        wayPointsDroneNS.add(new Checkpoint(SOUTH));
+
+        List<Checkpoint> wayPointsDroneWC = new ArrayList<>();
+        wayPointsDroneWC.add(new Checkpoint(CENTER));
+
+        new JavaTestKit(system) {
+            {
+                //create SimpleControlTower
+                final ActorRef simpleControlTower = system.actorOf(
+                        Props.create(SimpleControlTower.class,
+                                () -> new SimpleControlTower(getRef(),10,5,2))
+                );
+
+                //create CollisionDetector
+                final ActorRef collisionDetector = system.actorOf(
+                        Props.create(CollisionDetector.class,
+                                () -> new CollisionDetector(drones,getRef()))
+                );
+
+
+                simpleControlTower.tell(new StartFlightControlMessage(), getRef());
+
+                //start a new flight
+                simpleControlTower.tell(new AddFlightMessage(droneNS.getId(),wayPointsDroneNS),getRef());
+                simpleControlTower.tell(new AddFlightMessage(droneWC.getId(),wayPointsDroneWC),getRef());
+
+                expectMsgClass(MAX_DURATION_FLYING, WayPointCompletedMessage.class);
+                expectMsgAnyClassOf(MAX_DURATION_FLYING,WayPointCompletedMessage.class,FlightCompletedMessage.class);
+                expectMsgAnyClassOf(MAX_DURATION_FLYING,WayPointCompletedMessage.class,FlightCompletedMessage.class,RemoveFlightCompletedMessage.class);
+                expectMsgAnyClassOf(MAX_DURATION_FLYING,WayPointCompletedMessage.class,RemoveFlightCompletedMessage.class);
+                expectMsgClass(MAX_DURATION_FLYING, FlightCompletedMessage.class);
+
+                simpleControlTower.tell(new StopFlightControlMessage(), getRef());
+
+                //stop collision detector
+                collisionDetector.tell(new CollisionDetectorStopMessage(), getRef());
+                expectMsgEquals(MAX_DURATION_MESSAGE, false);
             }
         };
     }
